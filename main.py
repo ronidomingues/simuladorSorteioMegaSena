277f0afe1bc69726b12bcea32aa7ad9722d66835
Ronivaldo Domingues de Andrade <ronidomingues.ard@gmail.com>
2025-03-18 04:25:38 -0300

    fix: Update file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import requests;
+class Teste:
+    def __init__(self:object, name:str) -> None:
+        self.name = name;
+        pass;
+    def rename(self:object) -> str:
+        newName = input("Digite o novo nome: ");
+        print(self.name, newName, sep="\n");
+        pass;
+    pass;
+if __name__ == "__main__":
+    newObject = Teste("NameFantasy");
+    newObject.rename();
